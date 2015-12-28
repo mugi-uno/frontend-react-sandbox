c2a958bc297383709b70120a21e438a360939429
hmugish <hmugish@jops.co.jp>
2015-12-28 17:32:08 +0900

    add gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,2 +1,36 @@
 import gulp from 'gulp'
+import watch from 'gulp-watch'
+import webpack from 'gulp-webpack'
+import browser from 'browser-sync'
 
+gulp.task('webpack', () => {
+  return gulp.src('')
+    .pipe(webpack(require('./webpack.config.babel.js')))
+    .pipe(gulp.dest('public/'))
+})
+
+gulp.task('watch', () => {
+  gulp.watch([
+    'html/**/*',
+    'javascripts/**/*',
+    'stylesheets/**/*',
+  ], ['webpack'])
+});
+
+
+gulp.task('browser', () => {
+  browser({
+    server : {
+      baseDir : 'public/'
+    }
+  })
+});
+
+
+gulp.task('browser-reload', () => {
+  browser.reload();
+});
+
+gulp.task('default', ['watch', 'browser'], () => {
+  gulp.watch('public/**/*', ['browser-reload'])
+});
